e1c1f7711ceabf0aa467aed7bd103b44880edad6
YuChenHeMTL <yuchen7568@gmail.com>
2017-08-12 00:52:44 +0000

    deleted old migrations and created all tables

---
+++ db/migrate/20170812001511_create_users.rb
@@ -0,0 +1,47 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :password_digest
+      t.string :role
+    end
+
+    create_table :employers do |t|
+      t.belongs_to :user_id, index: true
+      t.text :description
+      t.string :icon_url
+    end
+
+    create_tale :applicants do |t|
+      t.belongs_to :user_id, index: true
+      t.integer :phone_number
+      t.text :description
+      t.string :resume_link
+      t.text :fields, array: true, default: []
+      t.boolean :paired
+    end
+
+    create_table :jobs do |t|
+      t.belongs_to :employers, index: true
+      t.string :position
+      t.text :description
+      t.text :requirements
+      t.string :job_type
+      t.text :apply_info
+      t.text :tags, array: true, default: []
+    end
+
+    create_table :students do |t|
+      t.string :name
+      t.string :concentration
+      t.string :industry
+      t.string :resume_link
+    end
+
+    create_table :students_applicants do |t|
+      t.belongs_to :applicants, index: true
+      t.belongs_to :students, index: true
+    end
+  end
+end
