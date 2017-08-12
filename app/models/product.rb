acde1daf0014134d9308187235f1962d9ea0e4ae
Sidney Cardozo <scooby123sid@gmail.com>
2017-08-12 20:08:09 +0000

    added enum role to User Model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,12 +1,8 @@
 class Product < ActiveRecord::Base
+  belongs_to :user
+  has_many :jobs
 
-  monetize :price_cents, numericality: true
-  mount_uploader :image, ProductImageUploader
-
-  belongs_to :category
-  has_many :reviews
-
-  validates :name, presence: true
+  validates :description, presence: true
   validates :price, presence: true
   validates :quantity, presence: true
   validates :category, presence: true
