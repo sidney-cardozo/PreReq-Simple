3ca24300e9ce08ece489b634c8a0848c4620e3cf
Sidney Cardozo <scooby123sid@gmail.com>
2017-08-14 22:44:51 +0000

    added relationships via the model between applicant, pair and student

--- app/models/applicant.rb
+++ app/models/applicant.rb
@@ -1,5 +1,6 @@
 class Applicant < ActiveRecord::Base
   belongs_to :user
+  has_many :pairs
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :description, presence: true
