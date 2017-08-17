cb9df71e19322cd27c54278d1ef3f0990fe6dc6c
YuChenHeMTL <yuchen7568@gmail.com>
2017-08-17 17:11:11 +0000

    added development changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,57 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
+
+puts "creating applicant users"
+
+User.destroy_all
+
+(10.times).each do
+User.create!({
+  email: Faker::Internet.email,
+  password: Faker::Internet.password(8),
+  role: 0
+  })
+end
+
+
+puts "applicant users created"
+
+puts "creating applicant profiles"
+
+Applicant.destroy_all
+
+User.all.each do |user|
+  Applicant.create!({
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    description: Faker::Hipster.paragraph(4),
+    resume_link: Faker::Internet.email,
+    user_id: user.id,
+    paired: false
+  })
+end
+
+puts "applicant profiles created"
 User.create!({
   email: "allo",
   password: "alloha",
   role: 2
+})
+
+puts "creating Students"
+Student.destroy_all
+
+(10.times).each do
+Student.create({
+  name: Faker::Name.first_name,
+  concentration: Faker::Hipster.word,
+  industry: Faker::Hipster.sentence,
+  resume_link: Faker::Internet.url,
+  paired: false
   })
+end
+
+puts "students created"
+
