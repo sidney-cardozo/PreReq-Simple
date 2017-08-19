cb23e89d20eac2d8353131d71ae4f72b83411366
YuChenHeMTL <yuchen7568@gmail.com>
2017-08-19 19:40:44 +0000

    added student page viewing restriction and link to applicant resume page

--- app/controllers/admin/students_controller.rb
+++ app/controllers/admin/students_controller.rb
@@ -1,5 +1,6 @@
 class Admin::StudentsController < ApplicationController
-  before_filter :authorize_admin
+  before_filter :authorize_admin, except: [:show]
+  before_filter :authorized_to_view, only: [:show]
 
   def create
     student = Student.create(student_params)
@@ -39,6 +40,22 @@ class Admin::StudentsController < ApplicationController
 
   def student_params
     params.require(:student).permit(:name, :email, :concentration, :industry, :resume_link, :image)
+  end
+
+  def authorized_to_view
+    if current_user
+      if @student = Student.find(params[:id])
+        if @pair = Pair.find_by_student_id(params[:id])
+          redirect_to "/", notice: "Access Denied" unless current_user.admin? || current_user == User.find(@pair.applicant.user_id)
+        else
+          redirect_to :back, notice: "Student is not paired !"
+        end
+      else
+        redirect_to :back, notice: "Student not existant !"
+      end
+    else
+      redirect_to "/", notice: "Please login to continue !"
+    end
   end
 
 end
