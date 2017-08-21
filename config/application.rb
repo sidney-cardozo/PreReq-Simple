a9406311705caae78d237b059017b398dad67585
Julie Rivest <julierivest93@gmail.com>
2017-08-21 23:31:02 +0000

    more styling done

--- config/application.rb
+++ config/application.rb
@@ -35,7 +35,7 @@ module Jungle
       g.assets       false
     end
 
-    config.serve_static_assets = false
+    # config.serve_static_assets = false
 
 # # Compress JavaScripts and CSS
 # config.assets.compress = true
@@ -47,6 +47,6 @@ module Jungle
 # config.assets.digest = true
 #     # config.assets.paths << "#{Rails.root}/app/assets/images"
 #     # Do not swallow errors in after_commit/after_rollback callbacks.
-#     config.active_record.raise_in_transactional_callbacks = true
+config.active_record.raise_in_transactional_callbacks = true
  end
 end
