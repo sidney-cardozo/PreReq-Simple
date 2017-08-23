ec9a47d8bd1bed77476671854ef93e85ae8ff125
Sidney Cardozo <scooby123sid@gmail.com>
2017-08-23 03:46:23 +0000

    Added Extensive Search Functionality

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,4 +1,5 @@
 class Job < ActiveRecord::Base
+  include PgSearch
   belongs_to :employer
   validates :position, presence: true
   validates :description, presence: true
@@ -6,14 +7,31 @@ class Job < ActiveRecord::Base
   validates :requirements, presence: true
   validates :job_type, presence: true
   validates :apply_info, presence: true
+  pg_search_scope :search,
+                  :against => {
+                    :description => 'A',
+                    :position => 'B'
+                    },
+                  :using => {
+                    :tsearch => {:dictionary => "english"}
+                  }
 
-  def self.search(query)
-    if query
-      where('description LIKE ?', "%#{query}%").order('updated_at DESC')
-    else
-      all.order('updated_at DESC')
-    end
-  end
+  # def self.search(query)
+  #   if query
+  #    words = query.to_s.strip.split
+  #    words.inject(scoped) do |combined_scope, word|
+  #     combined_scope.where('description ILIKE :search or position ILIKE :search', search: "%#{query}%")
+  #   end
+  #   else
+  #     all.order('updated_at DESC')
+  #   end
+  # end
 end
 
 
+# words = query.to_s.strip.split
+#     words.inject(scoped) do |combined_scope, word|
+#       combined_scope.where("quote LIKE ?", "%#{word}%")
+#     end
+
+# where('description ILIKE :search or position ILIKE :search', search: "%#{query}%").order('updated_at DESC')
